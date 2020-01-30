762437f68438fb0c849c211b64ba31ad4009faf6
Gregory Keeley <gregorykeeley@pursuit.org>
2020-01-30 13:01:39 -0500

    - Setup constraints for DetailViewController (Still troubleshooting)

--- Project/ProgrammaticUserCells/ProgrammaticUserCells/UserDetailView.swift
+++ Project/ProgrammaticUserCells/ProgrammaticUserCells/UserDetailView.swift
@@ -49,6 +49,12 @@ class UserDetailView: UIView {
     }
     private func commonInit() {
         setupUserImageConstraints()
+        setupNameLabelConstraints()
+        setupUserNameLabelConstraints()
+//        setupEmailConstraints()
+//        setupAgeLabelConstraints()
+//        setupLocationLabelConstraints()
+//        setupPhoneNumberLabelConstraints()
     }
     private func setupUserImageConstraints() {
         addSubview(userImageView)
@@ -58,8 +64,74 @@ class UserDetailView: UIView {
             userImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0),
             userImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
             userImageView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
-            userImageView.heightAnchor.constraint(equalTo: heightAnchor, constant: 0.40)
+            userImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.4)
             
         ])
     }
+    //nameLabel
+    private func setupNameLabelConstraints() {
+        addSubview(nameLabel)
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            nameLabel.topAnchor.constraint(equalTo: userImageView.bottomAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+        
+        ])
+    }
+    //userNameLabel
+    private func setupUserNameLabelConstraints() {
+        addSubview(userNameLabel)
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+
+            userNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            userNameLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20)
+
+        ])
+    }
+//    //locationLabel
+//    private func setupLocationLabelConstraints() {
+//        addSubview(locationLabel)
+//        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//
+//            locationLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 10),
+//            locationLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20)
+//
+//        ])
+//    }
+//    //phoneNumberLabel
+//    private func setupPhoneNumberLabelConstraints() {
+//        addSubview(phoneNumberLabel)
+//        phoneNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//
+//            phoneNumberLabel.topAnchor.constraint(equalTo: locationLabel.bottomAnchor, constant: 10),
+//            phoneNumberLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20)
+//
+//        ])
+//    }
+//    //emailLabel
+//    private func setupEmailConstraints() {
+//        addSubview(emailLabel)
+//        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//
+//            emailLabel.topAnchor.constraint(equalTo: phoneNumberLabel.bottomAnchor, constant: 10),
+//            emailLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20)
+//
+//        ])
+//    }
+//    //ageLabel
+//    private func setupAgeLabelConstraints() {
+//        addSubview(ageLabel)
+//        ageLabel.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//
+//            ageLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 10),
+//            ageLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20)
+//
+//        ])
+//    }
 }
